752340f94f626db04455b4a3dc12ce1650fea38b
Worachit1 <worachit.t@kkumail.com>
2022-11-28 15:40:08 +0700

    add nameID

---
+++ Lab-OOP-A/Lab-OOP-A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// print string commanline
+Console.WriteLine("Worachit Thonglert");
+//id
+Console.WriteLine("653450298-2");
+//email
+Console.WriteLine("worachit.t@kkumail.com");
